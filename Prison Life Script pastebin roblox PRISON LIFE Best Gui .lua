15a7204595f9f9c5ed42ab1e2be76676b394e627
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 06:52:14 +0100

    commit zero

---
+++ Prison Life Script pastebin roblox PRISON LIFE Best Gui .lua
@@ -0,0 +1,20 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://ghostbin.co/paste/rvu44/raw"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Functions:
+,Noclip
+,WalkSpeed
+,JumpPower
+,HipHeight
+,No Doors
+,Kill All
+,Arrest All
+,Get Item
+,Change Team
+						
+]]
